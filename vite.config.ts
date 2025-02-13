66b5f3defc5f0b2cacb4ae11a6af77d07fe5a39f
huracan <huracan@hahaha.com>
2025-02-13 22:55:04 +0100

    added proxy

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,5 @@ export default defineConfig({
         allowedHosts: [
             'stirred-llama-social.ngrok-free.app'
         ],
-        proxy: {
-            '/api': {
-                target: 'http://91.84.100.160:3000', // URL вашего бэкенда
-                changeOrigin: true,
-                secure: false,
-            },
-        },
     }
 })
