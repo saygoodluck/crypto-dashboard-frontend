b98a03e49b8560f13b5d9b8a20b24e52e093891c
huracan <huracan@hahaha.com>
2025-02-16 16:33:26 +0100

    cicd update

--- src/components/Telegram/TelegramAuthButton.tsx
+++ src/components/Telegram/TelegramAuthButton.tsx
@@ -10,7 +10,7 @@ const TelegramAuthButton = ({username}: { username: string | null }) => {
                 <a href={import.meta.env.VITE_TELEGRAM_BOT_URL} target="_blank" rel="noopener noreferrer">
                     <button className={"auth-button"}>
                         <img src="/telegram.svg" alt="Иконка" className={"tg-icon"} />
-                        Авторизоваться1
+                        Авторизоваться
                     </button>
                 </a>
             )}
